69e3529f777d2adf9048b4b8696a64b9ec9ce867
AlexTester01 <terzialexei81@gmail.com>
2022-09-30 10:29:39 +0300

    added cicle in script.js file

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var Услуги = ['Веб-дизайн', 'Разработка/создание сайтов', 'Раскрутка сайтов', 'SMM маркетинг'];
 
+function listУслуги(){
 for(var i=0; i < Услуги.length; i++){
     console.log(Услуги[i]);
 }
+}
+listУслуги();
+
+document.getElementById('box2').addEventListener('click',function(){alert('ОФОРМИТЬ ЗАКАЗ');});
